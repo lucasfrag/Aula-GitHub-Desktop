fd992a5d0677917e3256b2a556fef0b49ab0f439
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-03-07 21:06:23 -0300

    Corrigido problemas gerados pela má correção de conflito do GitHub

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -4,37 +4,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
-    public Connection connectDB(){
+
+    public Connection connectDB() {
         Connection conn = null;
-        
+
         try {
-<<<<<<< HEAD
-
             conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=q1w2e3r4");
-
         } catch (SQLException erro) {
-=======
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
->>>>>>> parent of 34a05e4 (Remoção de comentários e organização do código)
+
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
-    
+
 }
